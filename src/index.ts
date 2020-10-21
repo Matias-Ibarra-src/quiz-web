bcd4373b6277faf58f22848321c520c57c03b258
Matias-Zkas <matyas19981998@gmail.com>
2020-10-21 17:35:48 -0300

    primera

---
+++ src/index.ts
@@ -0,0 +1,59 @@
+function AlHacerClick(id: string){
+    var elemento=document.getElementById(id); 
+    //estos son de ciencias
+    if(elemento.id=='sy'){
+        alert("Maria Sibylla Meria n");
+    }
+    if(elemento.id=='st'){
+        alert("Maria Stopes");
+    }
+    if(elemento.id=='th'){
+        alert("Marie tharp y Sylva Earle");
+    }
+    if(elemento.id=='lh'){
+        alert("Inge Lehman");
+    }
+    if(elemento.id=='mh'){
+        alert("Wangari Maathai");
+    }
+    if(elemento.id=='cv'){
+        alert("Josefina Castellvi");
+    }
+    if(elemento.id=='ca'){
+        alert("Rachel Carson");
+    }
+    if(elemento.id=='av'){
+        alert("Ángeles Alvariño");
+    }
+    if(elemento.id=='ba'){
+        alert("Florence Bascom");
+    }
+
+    //estos son de ingenieria 
+
+    if(elemento.id=='ay'){
+        alert("Maria Sibylla Meria n");
+    }
+    if(elemento.id=='cb'){
+        alert("Maria Stopes");
+    }
+    if(elemento.id=='cr'){
+        alert("Marie tharp y Sylva Earle");
+    }
+    if(elemento.id=='ha'){
+        alert("Inge Lehman");
+    }
+    if(elemento.id=='ho'){
+        alert("Wangari Maathai");
+    }
+    if(elemento.id=='k'){
+        alert("Josefina Castellvi");
+    }
+    if(elemento.id=='la'){
+        alert("Rachel Carson");
+    }
+    if(elemento.id=='rr'){
+        alert("Ángeles Alvariño");
+    }
+
+}
