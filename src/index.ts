0d0eb38b269c3173de3928c4e8efcced597ddb4e
Matias-Zkas <matyas19981998@gmail.com>
2020-10-21 18:24:35 -0300

    segundo

--- src/index.ts
+++ src/index.ts
@@ -32,28 +32,144 @@ function AlHacerClick(id: string){
     //estos son de ingenieria 
 
     if(elemento.id=='ay'){
-        alert("Maria Sibylla Meria n");
+        alert("Hertha Ayrton");
     }
     if(elemento.id=='cb'){
-        alert("Maria Stopes");
+        alert("Pilar Carbonero");
     }
     if(elemento.id=='cr'){
-        alert("Marie tharp y Sylva Earle");
+        alert("Josephin Cochrane");
     }
     if(elemento.id=='ha'){
-        alert("Inge Lehman");
+        alert("Margaret Hamilton");
     }
     if(elemento.id=='ho'){
-        alert("Wangari Maathai");
+        alert("Grace Hopper");
     }
     if(elemento.id=='k'){
-        alert("Josefina Castellvi");
+        alert("Stephanie Kwolek");
     }
     if(elemento.id=='la'){
-        alert("Rachel Carson");
+        alert("Hedy Lamarr");
     }
     if(elemento.id=='rr'){
-        alert("Ángeles Alvariño");
+        alert("Ángela Ruiz Robles");
     }
 
+    //matematicas
+
+    if(elemento.id=='ag'){
+        alert("Maria Agnessi");
+    }
+    if(elemento.id=='bs'){
+        alert("Laura Bassi");
+    }
+    if(elemento.id=='by'){
+        alert("Pilar Bayer");
+    }
+    if(elemento.id=='c'){
+        alert("Emilie du Chätelet");
+    }
+    if(elemento.id=='cl'){
+        alert("M. Antónia Canals");
+    }
+    if(elemento.id=='cs'){
+        alert("M. Andrea Casamayor");
+    }
+    if(elemento.id=='ge'){
+        alert("Sophie Germain");
+    }
+    if(elemento.id=='gv'){
+        alert("Evelyn Boyd Granville");
+    }
+    if(elemento.id=='h'){
+        alert("Hipatia de Alejandria");
+    }
+    if(elemento.id=='jh'){
+        alert("Katherine Johnson");
+    }
+    if(elemento.id=='ko'){
+        alert("Sofia Kovalevskaya");
+    }
+    if(elemento.id=='lo'){
+        alert("Ada Lovelace");
+    }
+    if(elemento.id=='lp'){
+        alert("Nicole-Reine de Lepaute");
+    }
+    if(elemento.id=='mz'){
+        alert("Maryam Mirzajani");
+    }
+    if(elemento.id=='no'){
+        alert("Emmy Noether");
+    }
+    if(elemento.id=='sm'){
+        alert("Mary Sommerville");
+    }
+    if(elemento.id=='wo'){
+        alert("María Wonenburger");
+    }
+
+    //paleontologas
+    if(elemento.id=='an'){
+        alert("Mary Anning");
+    }
+    if(elemento.id=='lk'){
+        alert("Mary Leakey");
+    }
+    if(elemento.id=='ma'){
+        alert("María Martinón Torres");
+    }
+
+    //primatologas
+    if(elemento.id=='gd'){
+        alert("Jane Googall, Dian Fossey & Biruté Galdikas");
+    }
+
+    //quimicas
+    if(elemento.id=='al'){
+        alert("Frances H. Arnold");
+    }
+    if(elemento.id=='bb'){
+        alert("Katharine Burr Blodgett");
+    }
+    if(elemento.id=='bn'){
+        alert("Dorotea Barnés y las quimicas españolas de la edad de plata");
+    }
+    if(elemento.id=='cw'){
+        alert("Dorothy Crowfoot Hodgkin");
+    }
+    if(elemento.id=='fr'){
+        alert("Rosalind Franklin");
+    }
+    if(elemento.id=='fu'){
+        alert("Gertrudis de la Fuente");
+    }
+    if(elemento.id=='ju'){
+        alert("Manuela Juárez");
+    }
+    if(elemento.id=='ln'){
+        alert("Kathleen Lonsdale");
+    }
+    if(elemento.id=='me'){
+        alert("Marie Meurdrac & Jane Marcet");
+    }
+    if(elemento.id=='mn'){
+        alert("Rosa M. Menéndez");
+    }
+    if(elemento.id=='nd'){
+        alert("ida Noddack");
+    }
+    if(elemento.id=='pc'){
+        alert("Agnes Pockels");
+    }
+    if(elemento.id=='pz'){
+        alert("Marie Anne Paulze");
+    }
+    if(elemento.id=='rc'){
+        alert("Ellen Richards");
+    }
+    if(elemento.id=='yn'){
+        alert("Ada Yonath");
+    }
 }
